dc4e553948f7ce809f82171eb4e87b27c294b610
Weiwei Zhang <zhangweiwei0531@gmail.com>
2021-03-07 15:05:01 -0800

    Add ComponentScan

--- src/main/java/com/dynamo/DynamoApplication.java
+++ src/main/java/com/dynamo/DynamoApplication.java
@@ -2,8 +2,10 @@ package com.dynamo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com")
 public class DynamoApplication {
 
 	public static void main(String[] args) {
